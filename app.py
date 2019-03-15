caf9269dbd3e181860af64a1629aa073fce3ba5f
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2019-03-15 20:55:53 +0100

    Fix missing double quote in response

--- app.py
+++ app.py
@@ -6,17 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
-	
-@app.route('/method',methods = ['GET', 'POST', 'PUT', 'DELETE'])
+
+
+@app.route('/method', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def method():
-    return f'{request.method}' 
-	
+    return f'{request.method}'
+
+
 @app.route('/show_data', methods=['POST'])
 def show():
-    #join = request.args.get('join')
-    join = request.get_json()
-    return str(join)
+    # json = request.args.get('json')
+    json = request.get_json()
+    return str(json).replace("\'", "\"")
 
-	
+
 if __name__ == '__main__':
     app.run(debug=True)
