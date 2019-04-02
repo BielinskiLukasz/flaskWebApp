a3d69aec86fd202bf92d75806741dcef94e86d5f
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2019-04-02 21:35:31 +0200

    Order cities by name

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ def close_connection(exception):
 def tracks_list():
     db = get_db()
     cursor = db.cursor()
-    data = cursor.execute('SELECT name FROM tracks').fetchall()
+    data = cursor.execute('SELECT name FROM tracks ORDER BY name').fetchall()
     cursor.close()
     return render_template('tracks.html', tracks=data)
 
